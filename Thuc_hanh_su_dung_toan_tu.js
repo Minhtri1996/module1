36d5307f5a03debe9e64f9c67294cfee0d089d2f
Minhtri1996 <86427243+Minhtri1996@users.noreply.github.com>
2021-06-28 23:53:07 +0700

    Add files via upload

---
+++ Thuc_hanh_su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let nhapdai;
+let nhaprong;
+nhapdai=prompt("nhập dài :");
+nhaprong=prompt("nhập rộng:");
+let dai=parseInt(nhapdai);
+let rong=parseInt(nhaprong);
+let dientich=dai*rong;
+document.write("diện tích : "+dientich);
